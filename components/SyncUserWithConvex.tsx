09aadfce358cbd146da2a2e588c29790f4a83d25
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-30 10:06:09 +0100

    Creare SyncUserWithConvex component, to sync user with backend

---
+++ components/SyncUserWithConvex.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+const SyncUserWithConvex = () => {
+  const { user } = useUser();
+  // const updateUser = useMutation(api.users.updateUser);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const syncUser = async () => {
+      try {
+        await updateUser({
+          userId: user.id,
+          name: user.fullName,
+          email: user.emailAddresses[0]?.emailAddress ?? "",
+        });
+      } catch (error) {
+        console.log("Error, syncing user:", error);
+      }
+    };
+
+    syncUser();
+  }, [user, updateUser]);
+
+  return null;
+};
+
+export default SyncUserWithConvex;
