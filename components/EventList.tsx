ad3e5d90a4ae6a3b6922870caedf35521eb6754e
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-30 12:21:28 +0100

    Create Spinner component for wait wil events are loading

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import Spinner from "./Spinner";
+
 const EventList = () => {
+  const events = useQuery(api.events.get);
+
+  if (!events) {
+    return (
+      <div className="min-h-[400] flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
   return <div>EventList</div>;
 };
 
---
+++ convex/events.ts
@@ -0,0 +1,13 @@
+import { query } from "./_generated/server";
+// import { mutation } from "./_generated/server";
+// import {ConvexError, v} from "convex/values";
+
+export const get = query({
+    args: {},
+    handler: async (ctx) => {
+        return await ctx.db
+        .query("events")
+        .filter((q) => q.eq(q.field("is_cancelled"), undefined))
+        .collect();
+    },
+});
