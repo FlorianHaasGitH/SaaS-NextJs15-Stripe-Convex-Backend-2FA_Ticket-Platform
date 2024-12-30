a82f2b936073b058638fa3b7e56be222624e3bc8
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-30 19:12:02 +0100

    Create constants file for for fixed constants

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/router";
 
-const EventCard = () => {
+const EventCard = ({ eventId }: { eventId: Id<"events"> }) => {
+  const { user } = useUser();
+  const router = useRouter();
+  const event = useQuery(api.events.getById, { eventId });
+
   return <div>EventCard</div>;
 };
 
--- convex/events.ts
+++ convex/events.ts
@@ -1,6 +1,7 @@
 import { query } from "./_generated/server";
-// import { mutation } from "./_generated/server";
-// import {ConvexError, v} from "convex/values";
+import { v } from "convex/values";
+
+import { TICKET_STATUS } from "./constants";
 
 export const get = query({
     args: {},
@@ -9,5 +10,43 @@ export const get = query({
         .query("events")
         .filter((q) => q.eq(q.field("is_cancelled"), undefined))
         .collect();
+    },
+});
+
+export const getById = query({
+    args: { eventId: v.id("events") },
+    handler: async (ctx, { eventId }) => {
+      return await ctx.db.get(eventId);
+    },
+
+});
+
+export const getEventAvailability = query({
+    args: { eventId: v.id("events") },
+    handler: async (ctx, { eventId }) => {
+      const event = await ctx.db.get(eventId);
+      if (!event) throw new Error("Event not found");
+
+    // Count total purchased tickets
+    const purchasedCount = await ctx.db
+      .query("tickets")
+      .withIndex("by_event", (q) => q.eq("eventId", eventId))
+      .collect()
+      .then(
+        (tickets) =>
+          tickets.filter(
+            (t) =>
+              t.status === TICKET_STATUS.VALID ||
+              t.status === TICKET_STATUS.USED
+          ).length
+      );
+
+
+
+
+
+
+
+
     },
 });
